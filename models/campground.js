e5cf3b1a3a98cc7e9bf9bda5a993c129baa13df6
Sawyer <raistlinx@gmail.com>
2022-07-16 22:46:27 -0400

    added cluster map

--- models/campground.js
+++ models/campground.js
@@ -11,17 +11,19 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [ImageSchema],
+    images: [ ImageSchema ],
     geometry: {
         type: {
             type: String,
-            enum: ['Point'],
+            enum: [ 'Point' ],
             required: true
         },
         coordinates: {
-            type: [Number],
+            type: [ Number ],
             required: true
         }
     },
@@ -38,8 +40,13 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-});
+}, opts);
 
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
 
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -11,34 +11,37 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = array => array[ Math.floor(Math.random() * array.length) ];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for(let i = 0; i < 50; i++) {
+  for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: '62c8f5be66a1415349591873',
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      location: `${cities[ random1000 ].city}, ${cities[ random1000 ].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
       description: 'A spicy place!',
       price,
       geometry: {
         type: "Point",
-        coordinates: [-113.1331, 47.0202]
-        },
+        coordinates: [
+          cities[ random1000 ].longitude,
+          cities[ random1000 ].latitude
+        ]
+      },
       images: [
         {
-            url: 'https://res.cloudinary.com/cherrycoke/image/upload/v1657845652/YelpCamp/cwixxjvzqxfrukq0selx.jpg',
-            filename: 'YelpCamp/cwixxjvzqxfrukq0selx'
+          url: 'https://res.cloudinary.com/cherrycoke/image/upload/v1657845652/YelpCamp/cwixxjvzqxfrukq0selx.jpg',
+          filename: 'YelpCamp/cwixxjvzqxfrukq0selx'
         },
         {
-            url: 'https://res.cloudinary.com/cherrycoke/image/upload/v1657845652/YelpCamp/i9gxo4heuaof3rb4qn1h.jpg',
-            filename: 'YelpCamp/i9gxo4heuaof3rb4qn1h'
+          url: 'https://res.cloudinary.com/cherrycoke/image/upload/v1657845652/YelpCamp/i9gxo4heuaof3rb4qn1h.jpg',
+          filename: 'YelpCamp/i9gxo4heuaof3rb4qn1h'
         }
-    ]
+      ]
     })
     await camp.save();
   }
