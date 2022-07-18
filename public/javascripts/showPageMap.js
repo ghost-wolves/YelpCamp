5030beefea2ed82899e9b13b94d685f84ffa799c
Sawyer <raistlinx@gmail.com>
2022-07-17 22:54:53 -0400

    add styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,10 @@
 
-const goodCamp = JSON.parse(campground);
 
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: goodCamp.geometry.coordinates, // starting position [lng, lat]
+        center: campground.geometry.coordinates, // starting position [lng, lat]
         zoom: 5, // starting zoom
         projection: 'globe' // display the map as a 3D globe
     });
@@ -13,12 +12,14 @@ const goodCamp = JSON.parse(campground);
         map.setFog({}); // Set the default atmosphere style
     });
 
+map.addControl(new mapboxgl.NavigationControl());
+
     new mapboxgl.Marker()
-    .setLngLat(goodCamp.geometry.coordinates)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${goodCamp.title}</h3><p>${goodCamp.location}</p>`
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
             )
     )
     .addTo(map)
